60778df44bb699901cb631bb0867f2f5fa56f591
MalinduRK <79127305+MalinduRK@users.noreply.github.com>
2023-09-25 12:17:39 +0530

    Enabled hover effect on tower plots
    -I can't believe I finally made it work!
    -I had to make sure that the script and the 2D collider was attached to the main prefab object and not its individual components

---
+++ Assets/Scripts/HoverDetection.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class HoverDetection : MonoBehaviour
+{
+    private GameObject towerPlotHighlight; // Child object
+
+    private void Start()
+    {
+        // Find the child object by its name, relative to the parent.
+        towerPlotHighlight = transform.Find("TowerPlotHighlight").gameObject;
+        towerPlotHighlight.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        towerPlotHighlight.SetActive(true);
+        HoverDebug("Hovering over tower plot");
+    }
+
+    void OnMouseExit()
+    {
+        towerPlotHighlight.SetActive(false);
+        HoverDebug("Exited hovering over tower plot");
+    }
+
+    //--Debugs
+    public bool hoverDebug;
+
+    void HoverDebug(string message)
+    {
+        Debug.Log(message);
+    }
+}
