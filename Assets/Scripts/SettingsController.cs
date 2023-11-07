706ef4946670e0be3e7ce278b736f206cc2af1fa
MalinduRK <79127305+MalinduRK@users.noreply.github.com>
2023-11-07 10:41:02 +0530

    Created all 3 volume settings
    -All 3 volume settings in settings menu work now.
    -Each in-game sfx is also output via the SFX channel in the audio mixer.
    -Changing the master channel affects other two channels as well.

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class SettingsController : MonoBehaviour
 {
     [SerializeField] GameObject masterVolumeSlider;
+    [SerializeField] GameObject bgmVolumeSlider;
+    [SerializeField] GameObject sfxVolumeSlider;
 
     public AudioMixer audioMixer;
 
@@ -12,14 +14,23 @@ public class SettingsController : MonoBehaviour
 
     private void Start()
     {
+        // Variables to store current volumes
         float currentMasterVolume;
+        float currentBGMVolume;
+        float currentSFXVolume;
+
         // Set the volume sliders to the current volume of the audio mixer on load
         audioMixer.GetFloat("masterVolume", out currentMasterVolume);
+        audioMixer.GetFloat("bgmVolume", out currentBGMVolume);
+        audioMixer.GetFloat("sfxVolume", out currentSFXVolume);
 
         // Get the min value of the slider
         volumeMinValue = masterVolumeSlider.GetComponent<Slider>().minValue;
 
-        // Make sure the master volume stays at -80 if the slider is all the way down
+        // Make sure all the volumes stay at -80 if the sliders are all the way down
+        SettingsDebug($"Master volume: {currentMasterVolume} | BGM volume: {currentBGMVolume} | SFX volume: {currentSFXVolume}");
+
+        // Master
         if (currentMasterVolume > volumeMinValue)
         {
             masterVolumeSlider.GetComponent<Slider>().value = currentMasterVolume;
@@ -28,9 +39,31 @@ public class SettingsController : MonoBehaviour
         {
             masterVolumeSlider.GetComponent<Slider>().value = volumeMinValue;
         }
+
+        // BGM
+        if (currentBGMVolume > volumeMinValue)
+        {
+            bgmVolumeSlider.GetComponent<Slider>().value = currentBGMVolume;
+        }
+        else
+        {
+            bgmVolumeSlider.GetComponent<Slider>().value = volumeMinValue;
+        }
+
+        // SFX
+        if (currentSFXVolume > volumeMinValue)
+        {
+            sfxVolumeSlider.GetComponent<Slider>().value = currentSFXVolume;
+        }
+        else
+        {
+            sfxVolumeSlider.GetComponent<Slider>().value = volumeMinValue;
+        }
     }
 
-    public void SetVolume(float volume)
+    // Below are the functions to set volumes using sliders
+
+    public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("masterVolume", volume);
 
@@ -38,6 +71,41 @@ public class SettingsController : MonoBehaviour
         if (volume == volumeMinValue)
         {
             audioMixer.SetFloat("masterVolume", -80);
+        }
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        audioMixer.SetFloat("bgmVolume", volume);
+
+        // Mute if the slider is all the way down
+        if (volume == volumeMinValue)
+        {
+            audioMixer.SetFloat("bgmVolume", -80);
+        }
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("sfxVolume", volume);
+
+        // Mute if the slider is all the way down
+        if (volume == volumeMinValue)
+        {
+            audioMixer.SetFloat("sfxVolume", -80);
+        }
+    }
+
+
+    [Header("Debug")]
+
+    [SerializeField] private bool settingsDebug;
+
+    private void SettingsDebug(string message)
+    {
+        if (settingsDebug)
+        {
+            Debug.Log(message);
         }
     }
 }
