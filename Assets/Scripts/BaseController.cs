0427a1410058b360b41edafa09fbf9ccdffdf130
MalinduRK <79127305+MalinduRK@users.noreply.github.com>
2023-11-13 14:06:56 +0530

    The base now emits particles depending on how damaged it is

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -11,14 +11,31 @@ public class BaseController : MonoBehaviour
 
     [Header("Components")]
     private AudioSource audioSource;
+    public ParticleSystem damageParticleSystem;
+    private ParticleSystem.EmissionModule emissionModule;
 
     [Header("Variables")]
     public float rotationSpeed = 45.0f; // Adjust the rotation speed as needed.
+    private int baseHPValue; // Get current base HP
+    float damageEmissionRate = 0; // Starting emission of the base damage particles
+    float damageEmissionPerEnemy; // This is the amount of emission increase per enemy breaching the base
 
     private void Start()
     {
         // Assign audio source
         audioSource = GetComponent<AudioSource>();
+
+        // Assign starting base HP
+        baseHPValue = int.Parse(baseHP.text);
+
+        // Higher the base HP, lesser the damage emission one breach does to the base
+        damageEmissionPerEnemy = 100 / baseHPValue;
+
+        // Assign emission module of the particle system
+        emissionModule = damageParticleSystem.emission;
+
+        // Set the starting emission rate
+        emissionModule.rateOverTime = damageEmissionRate;
     }
 
     private void Update()
@@ -29,11 +46,11 @@ public class BaseController : MonoBehaviour
 
     public void BaseBreach()
     {
-        // Get current base HP
-        int baseHPValue = int.Parse(baseHP.text);
         // Reduce base HP value by 1
         baseHPValue--;
         baseHP.text = baseHPValue.ToString();
+        // Update damage emission
+        AdjustEmissionRate();
 
         // Game Over if base HP == 0
         if (baseHPValue == 0)
@@ -48,5 +65,13 @@ public class BaseController : MonoBehaviour
         // Play audio
         audioSource.clip = baseBreach;
         audioSource.Play();
+    }
+
+    private void AdjustEmissionRate()
+    {
+        damageEmissionRate += damageEmissionPerEnemy;
+
+        // Set the emission rate
+        emissionModule.rateOverTime = damageEmissionRate;
     }
 }
