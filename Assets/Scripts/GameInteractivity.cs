f31e6030cbbcee293ced6d70cb73f0414e6c8106
MalinduRK <79127305+MalinduRK@users.noreply.github.com>
2023-11-16 13:23:02 +0530

    Enemy spawner is no longer interactable while escape menu or game over overlay is open

--- Assets/Scripts/GameInteractivity.cs
+++ Assets/Scripts/GameInteractivity.cs
@@ -5,6 +5,7 @@ public class GameInteractivity : MonoBehaviour
 {
     [Header("Game Objects")]
     [SerializeField] private GameObject towerPlotsParent;
+    [SerializeField] private GameObject enemySpawner;
     private Transform[] towerPlots;
 
     public static bool interactionsDisabled = false; // This variable is useful for mouse enter/exit events
@@ -23,21 +24,29 @@ public class GameInteractivity : MonoBehaviour
     {
         interactionsDisabled = true;
 
+        // Tower plots
         foreach (Transform towerPlot in towerPlots)
         {
             // Get polygon collider and disable it
             towerPlot.GetComponent<PolygonCollider2D>().enabled = false;
         }
+
+        // Enemy spawner
+        enemySpawner.GetComponent<BoxCollider2D>().enabled = false;
     }
 
     public void EnableInteractions()
     {
         interactionsDisabled = false;
 
+        // Tower plots
         foreach (Transform towerPlot in towerPlots)
         {
             // Get polygon collider and disable it
             towerPlot.GetComponent<PolygonCollider2D>().enabled = true;
         }
+
+        // Enemy spawner
+        enemySpawner.GetComponent<BoxCollider2D>().enabled = true;
     }
 }
